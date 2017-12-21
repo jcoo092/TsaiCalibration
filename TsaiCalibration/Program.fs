9fc3d01d86d51356b2b094c7b1714b48eb32daf7
James Cooper <jcoo092@UoA.auckland.ac.nz>
2017-12-21 16:49:15 +1300

    Updated to fill in the extractCameraParams function with some dummy code.  Probably should just change all this to using the csv type provider stuff.

--- TsaiCalibration/Program.fs
+++ TsaiCalibration/Program.fs
@@ -16,20 +16,29 @@ Steps for Tsai Calibration:
 
 //exception FileExtensionNotValid of string
 
+type CameraParams = {pixelSize: double; width: int; height: int}
+
+let extractCameraParams cameraparamsfile = 
+    match System.IO.Path.GetExtension(cameraparamsfile) with
+    | ".csv" ->
+        let rawLines = File.ReadAllLines(cameraparamsfile)
+        {pixelSize = System.Double.Parse(rawLines.[0]); width = 640; height = 480}
+    | _ -> failwith "invalid file type provided"
+
 type CalibrationPoint = {xw: int; yw: int; zw: int; xd: int; yd: int}
 
 let extractCalibrationPoints calibpointsfile = 
     match System.IO.Path.GetExtension(calibpointsfile) with
     | ".csv" ->
         let rawLines = File.ReadAllLines(calibpointsfile)
-        if rawLines.[0] System.String
-        let splitLines = Array.map (fun (x : string) -> x.Split([|','|]) |> Array.map System.Int32.Parse) rawLines
-        Array.map (fun (x : int[]) -> {xw = x.[0]; yw = x.[1]; zw = x.[2]; xd = x.[3]; yd = x.[4]}) splitLines
+        let mutable splitLines = Array.map (fun (x : string) -> x.Split([|','|], System.StringSplitOptions.RemoveEmptyEntries)) rawLines
+        if not (fst (System.Int32.TryParse(splitLines.[0].[0]))) then
+            splitLines <- Array.skip 1 splitLines
+        let nums = Array.map (fun x -> Array.map System.Int32.Parse x) splitLines
+        Array.map (fun (x : int[]) -> {xw = x.[0]; yw = x.[1]; zw = x.[2]; xd = x.[3]; yd = x.[4]}) nums
     | _ -> failwith "invalid file type provided"
 
 let openAndLoadFiles cameraparamsfilename calibrationpointsfilename = 
-    //let cameraparamsfile = File.Open(argv.[0], FileMode.Open, FileAccess.Read)
-    //let calibrationpointsfile = File.Open(argv.[1], FileMode.Open, FileAccess.Read)
     let cameraparams = extractCameraParams cameraparamsfilename
     let calibrationpoints = extractCalibrationPoints calibrationpointsfilename
     (cameraparams, calibrationpoints)
